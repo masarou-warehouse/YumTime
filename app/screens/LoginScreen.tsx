31fcb17f78b111de6c64f8f8b4bc63201a0653e3
itsurboimasarou <medicspam6@gmail.com>
2024-11-25 19:07:43 +0700

    Building as Android app

--- app/screens/LoginScreen.tsx
+++ app/screens/LoginScreen.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { View, Text, Button, Image, Touchable, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import auth from '@react-native-firebase/auth';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
+// import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 const LoginScreen = () => {
-  const handleGoogleLogin = async () => {
-    try {
-      await GoogleSignin.signIn();
-      const { idToken } = await GoogleSignin.getTokens();
-      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-      await auth().signInWithCredential(googleCredential);
-    } catch (error) {
-      console.log('Google Signin Error', error);
-    }
-  };
+  // const handleGoogleLogin = async () => {
+  //   try {
+  //     await GoogleSignin.signIn();
+  //     const { idToken } = await GoogleSignin.getTokens();
+  //     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  //     await auth().signInWithCredential(googleCredential);
+  //   } catch (error) {
+  //     console.log('Google Signin Error', error);
+  //   }
+  // };
 
   return (
     <SafeAreaView className='flex-1' style={{backgroundColor: '#fff'}}>
